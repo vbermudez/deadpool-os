4c0dc5955cd5ea9d8709462f14942c1c240f62ef
Victor Bermudez <victor.bermudez@pearson.com>
2026-01-22 12:44:19 +0100

    fixes cors

--- server/index.js
+++ server/index.js
@@ -40,9 +40,7 @@ function initializeElevenLabs() {
 }
 
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? ['https://your-production-domain.com'] 
-    : ['http://localhost:5173', 'http://localhost:3000'],
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 
